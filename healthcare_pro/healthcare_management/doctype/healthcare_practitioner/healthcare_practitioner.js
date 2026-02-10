25ce62b982368d5e24b61307b4d33dce686a1c95
nelsonatuya <nelsonatuya07@gmail.com>
2026-02-10 10:26:38 +0300

    feat: added healthcare master doctypes

---
+++ healthcare_pro/healthcare_management/doctype/healthcare_practitioner/healthcare_practitioner.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Nelson Atuya and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Healthcare Practitioner", {
+// 	refresh(frm) {
+
+// 	},
+// });
